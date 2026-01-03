b378301e95ab549376754be031e8d7d6e649b80e
Divyam201218 <166046989+Divyam201218@users.noreply.github.com>
2026-01-03 22:54:26 +0530

    Update and rename main.js to _utils.js

---
+++ netlify/functions/_utils.js
@@ -0,0 +1,29 @@
+const admin = require("firebase-admin");
+
+const ALLOWED_ORIGIN = "https://badge-lookup.netlify.app";
+
+function corsHeaders(origin) {
+  if (origin === ALLOWED_ORIGIN) {
+    return {
+      "Access-Control-Allow-Origin": ALLOWED_ORIGIN,
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "POST, OPTIONS",
+    };
+  }
+  return {};
+}
+
+function initFirebase() {
+  if (!admin.apps.length) {
+    admin.initializeApp({
+      credential: admin.credential.cert(
+        JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+      ),
+      databaseURL: admin.credential.cert(JSON.parse(process.env.FIREBASE_URL)),
+
+    });
+  }
+  return admin.firestore();
+}
+
+module.exports = { corsHeaders, initFirebase };
